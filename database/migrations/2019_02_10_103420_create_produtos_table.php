7c846b48c351b7d8ec10bc1a9d1adaa6e238c385
Fernando Alves <alvesfernandosantos1@gmail.com>
2019-02-10 08:37:13 -0200

    php artisan make:migration create_produtos_table

---
+++ database/migrations/2019_02_10_103420_create_produtos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');    // sem nada nao pode ser nulo
+            $table->text('descricao')->nullable();  // nullable permite salvar nulo no banco 
+            $table->float('preco')->default(2); // se nao digitar nada, vai salvar 2 no banco
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
