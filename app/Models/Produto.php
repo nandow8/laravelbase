9174f53fe0ab79f3b0c9466b821c80aee39e1b81
Fernando Alves <alvesfernandosantos1@gmail.com>
2019-02-10 08:44:44 -0200

    softDelete(importante ter em todas tabelas)

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // toda vez que excluir, o produto nao aparece na lista, mas continua no banco de dados
 
 class Produto extends Model
 {
+    use SoftDeletes; // toda vez que excluir, o produto nao aparece na lista, mas continua no banco de dados
+
     protected $fillable = [   // se não passar os nomes dos campos aqui, nao vai conseguir salvar no banco ( id, created_at e updated_at nao precisa )
         'nome',
         'descricao',
--- database/migrations/2019_02_10_103420_create_produtos_table.php
+++ database/migrations/2019_02_10_103420_create_produtos_table.php
@@ -19,6 +19,7 @@ class CreateProdutosTable extends Migration
             $table->text('descricao')->nullable();  // nullable permite salvar nulo no banco 
             $table->float('preco')->default(2); // se nao digitar nada, vai salvar 2 no banco
             $table->timestamps();
+            $table->softDeletes(); // toda vez que excluir, o produto nao aparece na lista, mas continua no banco de dados
         });
     }
 
