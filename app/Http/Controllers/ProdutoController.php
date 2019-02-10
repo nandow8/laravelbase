d17a3bd17efe0a7afe0b6f655fea7515e96a4f10
Fernando Alves <alvesfernandosantos1@gmail.com>
2019-02-10 08:50:25 -0200

    criando metodo - rota - pagina.blade.php

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
-    //
+    public function index(){
+        $produto = Produto::all();
+        return view('normal.produtos.index', ['produto' => $produto]);
+    }
 }
